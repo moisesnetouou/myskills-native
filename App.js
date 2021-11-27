9de60b2afc4676478408e3ae69d77a5b53684299
Moisés <moisesnetored@gmail.com>
2021-11-27 09:57:43 -0400

    feat: Ajustes na organizaçaõ do projeto, retirando o default do componente

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-import React, {Fragment} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {Home} from './src/pages/Home';
 
 export default function App() {
-  return (
-    <Fragment>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>React Native</Text>
-      </View>
-      <Text style={{alignSelf: 'center', marginBottom: 20}}>Ignite</Text>
-    </Fragment>
-  );
+  return <Home />;
 }
